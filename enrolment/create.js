768070f6bfc4366548a7c2f7a9ad756612947ad9
Malek Erwin Karim Mohd Zin <erwinkarim@gmail.com>
2018-02-26 20:47:52 +0800

    Show invited courses * show invited courses from user side * think about how to handle enrolment when you already invited Update serverless config * update IAM role to access additional privs

--- enrolment/create.js
+++ enrolment/create.js
@@ -2,8 +2,24 @@ import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
+/*
+  enrol a user into the course
+  TODO: handle enrollment when you already invited.
+    plan: convert username into userId
+*/
 export async function main(event, context, callback) {
   const data = JSON.parse(event.body);
+
+  //if email context, find the current enrolment and update that instead
+  if(data.enrolmentContext){
+    if(data.enrolmentContext === 'email'){
+      //find the current enrolment record
+      //if found, update to current user id
+      //if not found, ignore this and let it create a new enrolment record
+
+    };
+  };
+
   const params = {
     TableName: "enrolment",
     Item: {
