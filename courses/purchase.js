fe76d27aea15a2d2288f6056a7434fd454ef20df
Malek Erwin Karim Mohd Zin <erwinkarim@gmail.com>
2018-04-18 09:59:43 +0800

    Add purchase fn

---
+++ courses/purchase.js
@@ -0,0 +1,22 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+const adyenEncrypt = require('adyen-cse-web');
+const options = {};
+const cseInstance = adyenEncrypt.createEncryption(
+  "B3CA83A5FF2F8E7B535325B076FABFBAA6FC3697C8298A479980AADCDD043912", 
+  options
+);
+
+
+/**
+ * purchase api using Adyen
+   plan: -
+   * create instance
+   * send data
+ */
+export async function main(event, context, callback) {
+  console.log('test purchase fn')
+
+  callback(null, success({}));
+};
